bf853906c326d17155c34440a8a49b23f53ed272
Bgoldfa <bgoldfa@gap.com>
2018-05-18 10:47:59 -0400

    Creating action for fetch_weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,12 @@
 import keys from '../../keys'
 
+//variable to hold action type
+export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 const API_KEY = keys.API_KEY
+
+export function fetchWeather(){
+    return {
+        type: FETCH_WEATHER
+    }
+}
