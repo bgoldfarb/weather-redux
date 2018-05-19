f7af1c39044acde0c3151fa4d98be20886dbe6a5
Bgoldfa <bgoldfa@gap.com>
2018-05-19 14:14:10 -0400

    creating weather list container and adding in city

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,6 +3,7 @@ import { FETCH_WEATHER } from '../actions/index'
 export default function(state = [], action){
     switch(action.type){
         case FETCH_WEATHER: 
+            //not mutating state here just adding to it, not like state.push(action.state.data)
             return [ action.payload.data, ...state ]
     }
     return state
