29c44ad4a39cfe8bafdae012e2680aa24325d424
Bgoldfa <bgoldfa@gap.com>
2018-05-19 21:19:10 -0400

    Adding google map images to city

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 class GoogleMap extends Component {
 
     componentDidMount(){
+        //creates new embedded google map 
+        //interacting with third party library in react
         new google.maps.Map(this.refs.map, {
+            //glimpse of city, screen width
             zoom: 12,
+            // where we want to center the map on
             center: {
                 lat: this.props.lat,
                 lng: this.props.lon
